b40e2a20d7cbc7fc982a242456e0025992db3efb
Kakha <kakha@hyve.gg>
2025-08-19 02:39:03 +0200

    new scene:

--- src/main.js
+++ src/main.js
@@ -13,49 +13,73 @@ const sizes = {
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 1000);
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
-//Loaders
-const textureLoader = new THREE.TextureLoader()
+//loaders
+const textureLoader = new THREE.TextureLoader();
+
+
+const textureMap = {
+  First: {
+    day: "/textures/Frist_Texture_Set_Deosed.webp"
+
+  },
+  Second: {
+    day: "/textures/Second_Texture_Set_Deosed.webp"
+  },
+  Third: {
+    day: "/textures/Third_Texture_Set_Deosed.webp"
+  }
+};
+const loadedTexture = {
+  First: {
+    day: {}
+  },
+  Second: {
+    day: {}
+  },
+  Third: {
+    day: {}
+  }
+};
+Object.entries(textureMap).forEach(([key, value]) => {
+  const dayTexture = textureLoader.load(value.day);
+  dayTexture.flipY = false;
+  dayTexture.colorSpace = THREE.SRGBColorSpace;
+  loadedTexture[key].day = dayTexture;
+  // const nightTexture = textureLoader.load(value.night);
+  // nightTexture.flipY = false;
+  // nightTexture.colorSpace = THREE.SRGBColorSpace;
+  // loadedTexture[key].night = nightTexture;
+});
+
+
+
 //model loader
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath('/draco/');
 
 const loader = new GLTFLoader();
 loader.setDRACOLoader(dracoLoader);
-
-const textureMap = {
-  First: {
-    day: "/textures/TextureSetDenoise_PNG.webp"
-
-  }
-};
-const loadedTexture = {
-  First: {
-    day: {}
-  }
-};
-
-Object.entries(textureMap).forEach(([key, value]) => {
-  const dayTexture = textureLoader.load(value.day)
-  dayTexture.flipY = false
-  dayTexture.colorSpace = THREE.SRGBColorSpace
-  loadedTexture[key].day = dayTexture
-});
-
-loader.load("/models/RoomV1.glb", (gltf) => {
+loader.load("/models/Room_V1-Compresed.glb", (gltf) => {
   gltf.scene.traverse((child) => {
     if (child.isMesh) {
-      const material = new THREE.MeshBasicMaterial()
-      material.map = loadedTexture.First.day
-      child.material = material
-      if (child.name === "Plane") {
-        const newMaterial = new THREE.MeshToonMaterial(
-          {
-            color: 0xffffff,
-            map: null
-          });
-        child.material = newMaterial;
+      if (child.name.includes("First")) {
+        const material = new THREE.MeshBasicMaterial()
+        material.map = loadedTexture.First.day
+        child.material = material
 
+      }
+      if (child.name.includes("Second")) {
+        const material = new THREE.MeshBasicMaterial()
+        material.map = loadedTexture.Second.day
+        child.material = material
+      }
+      if (child.name.includes("Third")) {
+        const material = new THREE.MeshBasicMaterial()
+        material.map = loadedTexture.Third.day
+        child.material = material
       }
       if (child.material.map) {
         child.material.map.minFilter = THREE.LinearFilter;
@@ -64,13 +88,19 @@ loader.load("/models/RoomV1.glb", (gltf) => {
   })
   scene.add(gltf.scene)
 })
+//model loader end
 
+
+
+
+//renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
 
 camera.position.set(5, 5.5, -11.7)
 const controls = new OrbitControls(camera, canvas)
@@ -80,13 +110,8 @@ controls.enablePan = true
 controls.dampingFactor = 0.5
 controls.target.set(1.4, 2.1, -1.6)
 
-
-//event listeners
-window.addEventListener("resize", OnResize);
-
-
-function OnResize() 
-{
+//event functions
+function OnResize() {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
   camera.aspect = sizes.width / sizes.height;
@@ -95,13 +120,54 @@ function OnResize()
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 }
 
+function OnMouseMove(event) {
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
 
+//event listeners
+window.addEventListener("mousemove", (e) => { OnMouseMove(e); })
+window.addEventListener("resize", OnResize);
 
+// update loop
 const Update = () => {
-  controls.update()
+  controls.update();
   renderer.render(scene, camera);
+  raycaster.setFromCamera(pointer, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  for (let i = 0; i < intersects.length; i++) {
+    if (intersects[i].object.name === "Book") {
+      PopObject(intersects[i].object);
+    }
+    if (intersects[i].object.name === "Drawer_Left") {
+      intersects[i].object.translateZ(-.5);
+    }
+  }
+  if (intersects.length > 0) {
+    // need to add ayerrs ike clickable, hoverable, animated, 
+    document.body.style.cursor = "pointer";
+  }
+  else {
+    document.body.style.cursor = "default";
+  }
+
+
   window.requestAnimationFrame(Update);
 }
 
+//functions
+function PopObject(object) {
+  console.log(object.name);
+  object.scale.x = 2;
+  object.scale.y = 2;
+  object.scale.z = 2;
+
+  setTimeout(() => {
+    object.scale.x = 1;
+    object.scale.y = 1;
+    object.scale.z = 1;
+  }, 1000);
+}
+//start update loop
 Update();
 
